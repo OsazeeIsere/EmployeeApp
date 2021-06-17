375e48b2b9930749217c32e50008e16101c381f4
Osazee Isere <oz1.concepts@gmail.com>
2021-06-17 15:21:33 +0100

    designing Enployee Class

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        //field data
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        //new field and property
+        private string empSSN;
+        private int empAge;
+        public int Age{get => empAge;set =>empAge = value;}
+        public string EmpSSN { get => empSSN; }
+        //constructors
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+           : this(name, 0, id, pay) { }
+        public Employee(string name,int age, int id,float pay)
+        {
+            empName = name;
+            empAge = age;
+            empID = id;
+            currPay = pay;
+        }
+
+        //methods
+        public void GiveBonus(float amount) => currPay += amount;//expression-bodied method
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"Name: {empName}");
+            Console.WriteLine($"Age:{empAge}");
+            Console.WriteLine($"ID:{empID}");
+            Console.WriteLine("Pay:{0}",currPay);
+        }
+        //Accessor(get method)
+        //public string GetName() => empName;
+
+        ////Mutator(se method)
+        //public void SetName(string name)
+        //{
+        //    //do a check before on incoming value 
+        //    //beforr making assignment
+        //    if (name.Length > 15)
+        //        Console.WriteLine("Error length of name exceed 15 character");
+        //    else
+        //        empName = name;
+
+        //}
+
+        //instead of using the accessor/mutator(get/set)
+        //methods to encapsulate each field data
+        //we use the .Net property(a simplification of get/set methods
+        //below are the properties
+        public string Name
+        { get { return empName; }
+        set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error length of name exceed 15 character");
+                else
+                    empName = Name;
+
+            }
+        }
+        //you could add additional business rules to the rest properties
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+    }
+}
--- EmployeeApp/Program.cs
+++ EmployeeApp/Program.cs
@@ -10,6 +10,18 @@ namespace EmployeeApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("=> fun with encapsulation");
+            Employee em = new Employee("Jason",233,50000);
+            em.DisplayStatus();
+
+            //use the get/set method to interact with the empName
+            em.Name=("Joan Oyenmwosa Isere");
+            Console.WriteLine($"The new employee is:{em.Name}");
+
+            Employee joe = new Employee();
+            joe.Age++;//this clean and allowed
+            Console.ReadLine();
+
         }
     }
 }
